9cd2fd99f86eebeef58574bf0208c1807876d059
Levon Poghosyan <levon9393@bk.ru>
2022-09-27 16:33:13 +0300

    Tests added for testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainIfLess1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIf1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIfMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+}
